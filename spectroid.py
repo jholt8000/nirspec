0920587d6b7ed40a78d3a374bdc5bfbe924833a4
jholt8000 <jen.holt1@gmail.com>
2014-11-22 14:25:43 -1000

    test

--- spectroid.py
+++ spectroid.py
@@ -16,28 +16,29 @@ Created on Thu Mar 28 10:30:41 2013
 import numpy as np
 from scipy import ndimage
 
-def spectroid(data_array, spw=10, bkw=30, dloc=926, trace_mean=True,
-              trace_last=False, trace_delta=1.9):
+
+def spectroid(data_array, spw=10, bkw=30, startingLocation=926, traceMean=True,
+              traceLast=False, traceDelta=1.9):
     """ Find centroids for each column in data_array -- only works horizontally
     
-     data_array: 2d numpy data array, usually read in from pyfits
-     spw=10: is distance away, up and down, from center to search for centroid
-     bkw=30: is background distance from center to subtract before finding centroid
-     dloc=y: starting location for centroid at x=0
-     trace_mean=True: if trace_mean is true and centroid found is 
-           >trace_delta*abs(previous centroid found) then use mean of previous 
+     :parameter data_array: 2d numpy data array, usually read in from pyfits
+     :parameter spw=10: is distance away, up and down, from center to search for centroid
+     :parameter bkw=30: is background distance from center to subtract before finding centroid
+     :parameter  startingLocation=y: starting location for centroid at x=0
+     :parameter traceMean=True: if traceMean is true and centroid found is
+           >traceDelta*abs(previous centroid found) then use mean of previous 
            3 centroids  
-           warning: only use trace_mean if curve to be fit does not have large jumps or 
+           warning: only use traceMean if curve to be fit does not have large jumps or 
            discontinuities
-     trace_last=False: if trace_last is true and centroid found is 
-          >trace_delta*abs(previous centroid found) then use previous centroid  
-           warning: only use trace_last if curve to be fit does not have large jumps or 
+     :parameter traceLast=False: if traceLast is true and centroid found is
+          >traceDelta*abs(previous centroid found) then use previous centroid  
+           warning: only use traceLast if curve to be fit does not have large jumps or 
            discontinuities
-           trace_last will take precedence over trace_mean if both are set to True
-     trace_delta: threshold for unacceptable jump in centroid location
+           traceLast will take precedence over traceMean if both are set to True
+     :parameter traceDelta: threshold for unacceptable jump in centroid location
      
-    returns: the centroid trace along the x-direction, the number of times the 
-    centroiding went outside of centroid[i-1]*trace_delta
+    :returns: the centroid trace along the x-direction, the number of times the
+    centroiding went outside of centroid[i-1]*traceDelta
     """
 
     # initialize "center of mass" array to be populated with centroids
@@ -45,15 +46,15 @@ def spectroid(data_array, spw=10, bkw=30, dloc=926, trace_mean=True,
     cm = np.zeros(data_array.shape[1])
 
     cm2 = []
-    #R -= R.sum(0) / len(R)
+    # R -= R.sum(0) / len(R)
 
     # badfit is a measure of how many times routine had to correct for bad centroid
     badfit = 0
 
     # first centroid will be starting location input parameter
-    # cm[0]=dloc
-    cm[0] = dloc
-    cm2.append(dloc)
+    # cm[0]=startingLocation
+    cm[0] = startingLocation
+    cm2.append(startingLocation)
     xmin = 1
     xmax = data_array.shape[1]
 
@@ -70,29 +71,29 @@ def spectroid(data_array, spw=10, bkw=30, dloc=926, trace_mean=True,
             ymax = int(data_array.shape[0])
 
         # if the min is smaller than the
-        #  bottom, use the bottom of array
-        if ymin < 1: #dont let it trace the bottom of the detector
+        # bottom, use the bottom of array
+        if ymin < 1:  # don't let it trace the bottom of the detector
             ymin = 1
         if ymax <= 0:
-            ymax = int(cm[i] + spw)+1
+            ymax = int(cm[i] + spw) + 1
 
         if bkw > 0:  # Subtract background
-            # bkmin is the center found for the previous column minus the bkw (background width)
+            # backgroundMin is the center found for the previous column minus the bkw (background width)
             # location of lower background sample
-            bkmin = cm[i - 1] - bkw
+            backgroundMin = cm[i - 1] - bkw
             # location of top background sample
-            bkmax = cm[i - 1] + bkw
+            backgroundMax = cm[i - 1] + bkw
 
             # do not go outside of the array
-            if bkmax > data_array.shape[0]:
-                bkmax = data_array.shape[0] - 1
-            if bkmin < 0:
-                bkmin = 0
+            if backgroundMax > data_array.shape[0]:
+                backgroundMax = data_array.shape[0] - 1
+            if backgroundMin < 0:
+                backgroundMin = 0
 
             # find the values to subtract from the array 
-            bkmax_val = data_array[bkmax, i]
-            bkmin_val = data_array[bkmin, i]
-            bk_mean = (bkmax_val + bkmin_val) / 2.
+            backgroundMax_val = data_array[backgroundMax, i]
+            backgroundMin_val = data_array[backgroundMin, i]
+            bk_mean = (backgroundMax_val + backgroundMin_val) / 2.
 
         else:
             bk_mean = 0.0  # do not subtract background value
@@ -103,28 +104,27 @@ def spectroid(data_array, spw=10, bkw=30, dloc=926, trace_mean=True,
         # for each column (i)  find the sum (centroid_value_j * y_j) 
         # where centroid_value_j is the value at y=j,x=i         
 
-        cm[i] = ndimage.measurements.center_of_mass(data_array[int(ymin):int(ymax)+1, i] - bk_mean)[0] + ymin
+        cm[i] = ndimage.measurements.center_of_mass(data_array[int(ymin):int(ymax) + 1, i] - bk_mean)[0] + ymin
 
         # return data_array[:,i]
-        if cm[i] is np.inf or cm[i] is -np.inf : # This happens when we went off array
+        if cm[i] is np.inf or cm[i] is -np.inf:  # This happens when we went off array
             return np.array(0.0), 9999
 
-        # centroid jumped more than trace_delta
-        if np.abs(cm[i] - cm[i - 1]) > trace_delta :
+        # centroid jumped more than traceDelta
+        if np.abs(cm[i] - cm[i - 1]) > traceDelta:
             badfit += 1
-            if trace_mean:
+            if traceMean:
                 if i > 4:
                     # jump is past beginning, use past three centroids
-
-                    cm[i] = cm[i-3:i-1].mean()
+                    cm[i] = cm[i - 3:i - 1].mean()
                 elif i > 1:
                     # average as many cms as we have gone through
-                    cm[i] = cm[i-2:i-1].mean()
+                    cm[i] = cm[i - 2:i - 1].mean()
                 else:
                     # use the first one found
-                    cm[i] = cm[i-1]
+                    cm[i] = cm[i - 1]
 
-            elif trace_last:
+            elif traceLast:
                 cm[i] = cm[i - 1]
 
     return cm, badfit
